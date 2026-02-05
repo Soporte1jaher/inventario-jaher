affb4880962b3fd48b2d81987a9f6c64fe02d397
Soporte1jaher <soporte1@jaher.com.ec>
2026-02-05 14:07:20 -0500

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -336,7 +336,7 @@ with t1:
         
         df_editor = pd.DataFrame(st.session_state.draft)
         # Columnas obligatorias para visualización
-        cols_base = ["equipo","marca","modelo","serie","cantidad","estado","tipo","origen","destino","guia","fecha_llegada","reporte"]
+        cols_base = ["equipo","marca","modelo","serie","cantidad","estado","tipo","origen","destino","guia","fecha_llegada","ram","disco","procesador","reporte"]
         for c in cols_base:
             if c not in df_editor.columns: df_editor[c] = ""
         
