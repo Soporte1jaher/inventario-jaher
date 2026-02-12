9bc03c4283b95328764260e59725615e36fb5895
Soporte1jaher <soporte1@jaher.com.ec>
2026-02-12 18:23:48 -0500

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,2 +1,2 @@
-# ui/__init__.py
-LOGO_URL = "https://raw.githubusercontent.com/Soporte1jaher/inventario-jaher/main/assets/logo_jaher.png"
+self.LOGO_URL = "https://raw.githubusercontent.com/Soporte1jaher/inventario-jaher/main/assets/logo_jaher.png"
+self.LOGO_LOCAL_PATH = "assets/logo_jaher.png"
